c76c244fea2d474fa301658dc52227759cf4c8b2
Dakshesh Sharma <154011397+daksheshsharma2409@users.noreply.github.com>
2026-07-28 23:53:20 +0530

    [126] [Time Beats: 46.26%] [Memory Beats: 95.13%] - LeetPush

---
+++ 126-Word-Ladder-II.cpp
@@ -0,0 +1,59 @@
+class Solution {
+private:
+    void dfs(string word, vector<string>& seq, string& b){
+        if (word == b){
+            reverse(seq.begin(), seq.end());
+            ans.push_back(seq);
+            reverse(seq.begin(), seq.end());
+            return;
+        }
+        int steps = mpp[word];
+        for (int i=0; i<word.size(); i++){
+            char original = word[i];
+            for (char ch = 'a'; ch<='z'; ch++){
+                word[i] = ch;
+                if (mpp.find(word) != mpp.end() && mpp[word] + 1 == steps ){
+                    seq.push_back(word);
+                    dfs(word, seq, b);
+                    seq.pop_back();
+                }
+            }
+            word[i] = original;
+        }
+
+    }
+public:
+    unordered_map<string, int> mpp;
+    vector<vector<string>> ans;
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        queue<string> q;
+        q.push({beginWord});
+        mpp[beginWord] = 1;
+        int sz = beginWord.size();
+        st.erase(beginWord);
+        while (q.size()){
+            string word = q.front();
+            int steps = mpp[word];
+            q.pop();
+            if (word == endWord) break;
+            for (int i = 0; i<sz; i++){
+                char original = word[i];
+                for (char ch = 'a'; ch<='z'; ch++){
+                    word[i] = ch;
+                    if (st.find(word) != st.end()){
+                        q.push(word);
+                        st.erase(word);
+                        mpp[word] = steps+1;
+                    }
+                }
+                word[i] = original;
+            }
+        }
+        if (mpp.find(endWord) != mpp.end()){
+            vector<string> seq = {endWord};
+            dfs(endWord, seq, beginWord);
+        }
+        return ans;
+    }
+};
